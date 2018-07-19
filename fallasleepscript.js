716c01b2f5d5d85cc21e09fd4efee5df4ab19033
Seim <seimwolderufael@gmail.com>
2018-07-19 15:40:58 +0000

    First commit.

---
+++ fallasleepscript.js
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <script src="fallasleepscript.js"></script>
+    <link rel="stylesheet" href="fallasleepstyle.css" type="text/css">
+    <title>Zzleep</title>
+</head>
+<body>
+    <div id="header">
+        <h1>ZZleep</h1>
+    </div>
+    
+    <div id="main">
+        <button id="Music">
+            <h2>Music to help you sleep</h2>
+        </button>
+        <button id="BodyPosition">
+            <h2>Body positions for better sleeping</h2>
+        <button id="Breathing"
+            <h3>Breathing methods for sleeping</h3>
+    </div>
+    
+     <div id="section">
+        <div id="sectionleft">
+            
+        </div>
+        <div id="sectionmiddle">
+            
+        </div>
+        <div id="sectionright">
+            
+        </div>
+    </div>
+    <div id="footer">
+        
+    </div>
+</body> 
+</html>
---
+++ script.js
@@ -0,0 +1,3 @@
+var fall_button = document.querySelector('#fall')
+var plan_button = document.querySelector('#plan')
+
